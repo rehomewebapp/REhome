4f6eb129c5fb40f8a677355341d3c29dbd091539
Monisha1722 <74071063+Monisha1722@users.noreply.github.com>
2021-06-21 17:56:17 +0200

    Update app.py

--- WebApp/app.py
+++ WebApp/app.py
@@ -7,3 +7,5 @@ external_stylesheets=external_stylesheets,
                  suppress_callback_exceptions=True)
                  
 server = app.server
+
+#this script in required to be put in a seperate python file in case of building multipage apps. The styling of our webpage will also be mentioned here.
