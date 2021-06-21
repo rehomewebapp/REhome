8f55d0c7517d8bf20f0283b384060071beed66c6
Monisha1722 <74071063+Monisha1722@users.noreply.github.com>
2021-06-21 18:00:27 +0200

    Update index.py

--- WebApp/index.py
+++ WebApp/index.py
@@ -6,15 +6,18 @@ from app import app, server
 from views import home_view  ,location_view, about_us_view
 from controllers import location_controller
 
-
+# this sets the layout of our webpage, dcc.location sets the url of the page, and html.div returns the page content of all the child pages of our web app
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div(id='page-content')
 ])
 
+# the callback display_page receives the current pathname (the last part of the URL) of the page. The callback simply displays the pathname on page but it could use the pathname to display different content.
 
 @app.callback(Output('page-content', 'children'),
               Input('url', 'pathname'))
+
+# the below function, returns the layout of all the different scripts and has to be imported in line 5.
 def display_page(pathname):
     if pathname == '/':
         return home_view.layout
