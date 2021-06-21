07c9e2c2525162639c3d097ec2b0ee9315a353c6
Monisha1722 <74071063+Monisha1722@users.noreply.github.com>
2021-06-21 18:01:57 +0200

    Update about_us_view.py

--- WebApp/views/about_us_view.py
+++ WebApp/views/about_us_view.py
@@ -1,7 +1,8 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
-
+# every child page has a layout as shown below and can be run via the code index.py 
+# https://dash.plotly.com/introduction please go through this link to add more features in these cases
 layout = html.Div(children=[
     html.H1(children="About Us"),
     html.Br(),
