beb6c5b96d6ba0033d8331aa11a9193299dc6bcc
Taubenstrohhalm <6072616+Taubenstrohhalm@users.noreply.github.com>
2021-06-22 19:09:59 +0200

    add: link to floorplan

--- WebApp/views/location_view.py
+++ WebApp/views/location_view.py
@@ -1,7 +1,6 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
-from dash.dependencies import Input, Output, State
 
 from app import app
 
@@ -16,8 +15,10 @@ layout = html.Div(children=[
     dcc.Input(id="input", type="text", placeholder="Enter post code", value = ''),
     html.Button(children = 'Submit', id="submit_button", n_clicks=0),
     html.Div(id='location_output',children=''),
+    dcc.Link('Next',id="link_to_floorplan", href="/floorplan"),
     html.Br(),
     dcc.Link('Home', href="/"), # link to landing page
+    
 ])
 
 if __name__ == '__main__':
