dce9dcedf76e72e8b82944647e252dcbafff4184
suubi <67806224+ssuubi@users.noreply.github.com>
2021-06-26 15:45:41 +0200

    add: windmill to footer

--- WebApp/views/about_us_view.py
+++ WebApp/views/about_us_view.py
@@ -1,9 +1,11 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 
 from views.templates.footer import footer
 from views.templates.header import create_navbar
+from views.templates.sidebar import sidebar
 
 # every child page has a layout as shown below and can be run via the code index.py 
 # https://dash.plotly.com/introduction please go through this link to add more features in these cases
@@ -11,10 +13,21 @@ from views.templates.header import create_navbar
 navbar = create_navbar("About Us")
 ABOUT_US = "/assets/about_us.png"
 
+content = html.Div([
+    html.Br(),
+    html.P("We are a team of students who want to save the world (:"),
+    html.Img(src=ABOUT_US),
+])
+
 layout = html.Div(children=[
     navbar,
-    html.Br(),
-    html.P("We are a team of students who want to save the world."),
-    html.Img(src=ABOUT_US),
+    html.Div([
+        dbc.Row([
+            dbc.Col(sidebar,width = 2),
+            dbc.Col([
+                content,
+            ],width=10,style={"padding-left":"0px"})
+        ],style={"margin-right":"0px"}),
+    ]),
     footer,
 ])
--- WebApp/views/templates/footer.py
+++ WebApp/views/templates/footer.py
@@ -1,8 +1,10 @@
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 
+WINDMILL = "/assets/windmill.png"
 
 footer = html.Div(children = [
+    dbc.Col(html.Img(src=WINDMILL, height="150px"), style={"text-align" : "right"}),
     dbc.NavbarSimple(
         [
             dbc.NavItem(dbc.NavLink("About us", href="/views/about_us_view")),
