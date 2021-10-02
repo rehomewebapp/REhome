6c5446a4530def41f0a185a7f0c4992d97b21c0e
Taubenstrohhalm <6072616+Taubenstrohhalm@users.noreply.github.com>
2021-10-02 10:38:11 +0200

    edit: change save button color to green only after saving the location

--- WebApp/controllers/location_controller.py
+++ WebApp/controllers/location_controller.py
@@ -1,5 +1,6 @@
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
+from dash import callback_context
 import dash_leaflet as dl
 
 
@@ -8,47 +9,44 @@ from models.building import utilities
 from models.building.buildingFactory import read_building_data_yaml, save_building_data_yaml
 
 # create marker and change button style
+# save location data and download weather data
 @app.callback(
     Output("layer", "children"),
     Output('location_done_button_id', 'disabled'), 
     Output('location_done_button_id', 'color'), 
-    Input("map", "click_lat_lng")
+    Input("map", "click_lat_lng"),
+    Input('location_done_button_id','n_clicks'),
+    State("map", "click_lat_lng"),
     )
-def map_click(click_lat_lng):
-    if click_lat_lng == None:
+def map_click(click_lat_lng, n_clicks, click_lat_lng_state):
+    ctx = callback_context
+    # on page load read default loc data and set marker
+    if not ctx.triggered:
+        button_id = 'No clicks yet'
         location = list(read_building_data_yaml('userID')['location'].values())
         marker = dl.Marker(position=location)
-        #raise PreventUpdate()
+        return marker, False, "success"
+    # check which input has triggered the callback
     else:
+        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+    # handle click on map -> set marker position
+    if button_id == "map":
         marker = [dl.Marker(position=click_lat_lng)]
-    return marker, False, "success"
+        return marker, False, "primary"
+    # handle save button
+    elif button_id == "location_done_button_id":
+        print("save button pressed")
+        building = read_building_data_yaml('userID')
+        building["location"] = {
+            "latitude": click_lat_lng_state[0],
+            "longitude": click_lat_lng_state[1],
+        }
+        save_building_data_yaml(building)
+        marker = [dl.Marker(position=click_lat_lng)]
 
-
-# save location data and download weather data
-@app.callback(
-    Output('location_output','children'),
-    Input('location_done_button_id','n_clicks'),
-    State("map", "click_lat_lng")
-)
-def handle_location(n_clicks, loc):
-    if n_clicks == 0:
-        raise PreventUpdate()
-    if n_clicks == 0:
-        return ''
-    building = read_building_data_yaml('userID')
-    building["location"] = {
-        "latitude": loc[0],
-        "longitude": loc[1],
-    }
-    save_building_data_yaml(building)
-
-    data = utilities.get_tmy_data(loc[0],loc[1])
-    utilities.save_tmy_data(data)
-    utilities.read_tmy_data()
-    return ""
-
-
-
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+        # download weather data and save
+        data = utilities.get_tmy_data(loc[0],loc[1])
+        utilities.save_tmy_data(data)
+        return marker, False, "success"
+    else: 
+        print("This shoud never happen...")
