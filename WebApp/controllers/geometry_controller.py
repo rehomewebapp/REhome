49710470b9980eeb13b5a0079bf1eb780f9ef7c5
Taubenstrohhalm <6072616+Taubenstrohhalm@users.noreply.github.com>
2021-10-08 11:18:04 +0200

    add: input for windows

--- WebApp/controllers/geometry_controller.py
+++ WebApp/controllers/geometry_controller.py
@@ -5,18 +5,24 @@ from dash import callback_context
 from models.building import geometry
 from models.building.buildingFactory import read_building_data_yaml, save_building_data_yaml
 
-
-
 @app.callback(
     Output('geometry_done_button_id', 'disabled'), 
     Output('geometry_done_button_id', 'color'),
     Output("n_storys_id", "value"),
     Output("story_height_id", "value"),
+    Output("area_win_a_id", "value"),
+    Output("area_win_b_id", "value"),
+    Output("area_win_c_id", "value"),
+    Output("area_win_d_id", "value"),
     Input("n_storys_id", "value"),
     Input("story_height_id", "value"),
+    Input("area_win_a_id", "value"),
+    Input("area_win_b_id", "value"),
+    Input("area_win_c_id", "value"),
+    Input("area_win_d_id", "value"),
     Input("geometry_done_button_id", "n_clicks"),
 )
-def inputDone(n_storys, story_height, clicks):
+def inputDone(n_storys, story_height, area_win_a,area_win_b,area_win_c, area_win_d, clicks):
     ctx = callback_context
     # check which input has triggered the callback
     button_id = ctx.triggered[0]['prop_id'].split('.')[0]
@@ -26,11 +32,15 @@ def inputDone(n_storys, story_height, clicks):
         building = read_building_data_yaml("userID")
         n_storys = building['nStorys']
         story_height = building['thZones']['livingSpace']['storyHeight']
-        return False, "success", n_storys, story_height
+        area_win_a = building['thZones']['livingSpace']['transPlanes']['windowA']['area']
+        area_win_b = building['thZones']['livingSpace']['transPlanes']['windowB']['area']
+        area_win_c = building['thZones']['livingSpace']['transPlanes']['windowC']['area']
+        area_win_d = building['thZones']['livingSpace']['transPlanes']['windowD']['area']
+        return False, "success", n_storys, story_height, area_win_a, area_win_b, area_win_c, area_win_d
 
     # disable button if one input field is empty
-    if n_storys == None or story_height == None:
-        return True, "primary", n_storys, story_height
+    if not n_storys or not story_height or not area_win_a or not area_win_b or not area_win_c or not area_win_d:
+        return True, "primary", n_storys, story_height, area_win_a, area_win_b, area_win_c, area_win_d
 
     # save building data on button click
     if button_id == "geometry_done_button_id":
@@ -53,13 +63,17 @@ def inputDone(n_storys, story_height, clicks):
         building['thZones']['livingSpace']['opaquePlanes']['floor']['area'] = groundArea
         building['thZones']['livingSpace']['floorArea'] = heatedArea
         building['thZones']['livingSpace']['volume'] = volume
+        building['thZones']['livingSpace']['transPlanes']['windowA']['area'] = area_win_a
+        building['thZones']['livingSpace']['transPlanes']['windowB']['area'] = area_win_b
+        building['thZones']['livingSpace']['transPlanes']['windowC']['area'] = area_win_c
+        building['thZones']['livingSpace']['transPlanes']['windowD']['area'] = area_win_d
         save_building_data_yaml(building)
 
-        return False, "success", n_storys, story_height
+        return False, "success", n_storys, story_height, area_win_a, area_win_b, area_win_c, area_win_d
 
     # activate button if all inputs are filled
     else:
-        return False, "primary", n_storys, story_height
+        return False, "primary", n_storys, story_height, area_win_a, area_win_b, area_win_c, area_win_d
 
 
     
--- WebApp/views/geometry_view.py
+++ WebApp/views/geometry_view.py
@@ -4,12 +4,13 @@ import dash_bootstrap_components as dbc
 from views.templates.createLayout import create_layout
 
 
-input = html.Div(
-    [
+input = html.Div([   
+    html.H4("Stories"),
+    dbc.Row([
         dbc.Col(
             dbc.FormGroup(
                 [
-                    dbc.Label("Number of storys", html_for="n_storys_id"),
+                    dbc.Label("Number", html_for="n_storys_id"),
                     dbc.Input(
                         type="number",
                         id="n_storys_id",
@@ -17,11 +18,11 @@ input = html.Div(
                     ),
                 ]
             ),
-        ),
+        width=6),
         dbc.Col(
             dbc.FormGroup(
                 [
-                    dbc.Label("Height of the storys [m]", html_for="story_height_id"),
+                    dbc.Label("Height [m]", html_for="story_height_id"),
                     dbc.Input(
                         type="number",
                         step=0.01,
@@ -30,7 +31,59 @@ input = html.Div(
                     ),
                 ]
             ),
-        ),
+        width=6),
+    ]),
+    html.H4("Window area [m²]"),
+    dbc.Row([
+        dbc.Col(
+            dbc.FormGroup(
+                [
+                    dbc.Label("North", html_for="area_win_a_id"),
+                    dbc.Input(
+                        type="number",
+                        id="area_win_a_id",
+                        placeholder="Window area facing north",
+                    ),
+                ]
+            ),
+        width=3),
+        dbc.Col(
+            dbc.FormGroup(
+                [
+                    dbc.Label("East", html_for="area_win_b_id"),
+                    dbc.Input(
+                        type="number",
+                        id="area_win_b_id",
+                        placeholder="Window area facing east",
+                    ),
+                ]
+            ),
+        width=3),
+        dbc.Col(
+            dbc.FormGroup(
+                [
+                    dbc.Label("South", html_for="area_win_c_id"),
+                    dbc.Input(
+                        type="number",
+                        id="area_win_c_id",
+                        placeholder="Window area facing south",
+                    ),
+                ]
+            ),
+        width=3),
+        dbc.Col(
+            dbc.FormGroup(
+                [
+                    dbc.Label("West", html_for="area_win_d_id"),
+                    dbc.Input(
+                        type="number",
+                        id="area_win_d_id",
+                        placeholder="Window area facing west",
+                    ),
+                ]
+            ),
+        width=3),
+    ]),
     ],
 )
 
